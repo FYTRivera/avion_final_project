4faa3222b2e3d74bec9c80d3276e16d195ce5b7b
Franco Rivera <franco@LAPTOP-7R15A7O9.localdomain>
2022-09-20 20:39:11 +0800

    fix users and add meetings scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        #  :confirmable
+
+  validates :role, numericality: { greater_than_or_equal_to: 0, less_than: 2 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :meetings
   devise_for :users
+  resources :users
   root "homepage#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
