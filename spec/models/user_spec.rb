02d693b6e0664d98a9ce79fb958a9c6051ee38a3
Franco Rivera <franco@LAPTOP-7R15A7O9.localdomain>
2022-10-03 21:07:50 +0800

    add rspec tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
     it "is valid with valid attributes" do
         expect(User.create(email:"frari2000@gmail.com", password: "123456", first_name: "Juan", last_name: "Cruz", age: "18")).to be_valid
-      end
+    end
     
-      it "is not valid without a title"
-      it "is not valid without a description"
-      it "is not valid without a start_date"
-      it "is not valid without a end_date"
+    it "is not valid without an email" do
+        expect(User.create(email: nil, password: "123456", first_name: "Juan", last_name: "Cruz", age: "18")).to be_invalid
+    end
+    
+    it "is not valid without a password" do
+        expect(User.create(email: "frari2000@gmail.com", password: nil, first_name: "Juan", last_name: "Cruz", age: "18")).to be_invalid
+    end
+
+    it "is not valid without a first name" do
+        expect(User.create(email: "frari2000@gmail.com", password: "123456", first_name: nil, last_name: "Cruz", age: "18")).to be_invalid
+    end
+
+    it "is not valid without a last name" do
+        expect(User.create(email: "frari2000@gmail.com", password: "123456", first_name: "Juan", last_name: nil, age: "18")).to be_invalid
+    end
+
+    it "is not valid withohut an age" do
+        expect(User.create(email: "frari2000@gmail.com", password: "123456", first_name: "Juan", last_name: "Cruz", age: nil)).to be_invalid
+    end
 end
